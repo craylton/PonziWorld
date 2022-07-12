920ac2017b0629f5bcfdef7f1c24a04dc9ae86df
Simon Palmer <sipalmer123@gmail.com>
2022-07-12 15:51:02 +0100

    Use view model for company information

--- PonziWorld/CompanyInformation/CompanyInformationViewModel.cs
+++ PonziWorld/CompanyInformation/CompanyInformationViewModel.cs
@@ -1,5 +1,42 @@
-﻿namespace PonziWorld.CompanyInformation;
+﻿using Prism.Mvvm;
 
-internal class CompanyInformationViewModel
+namespace PonziWorld.CompanyInformation;
+
+internal class CompanyInformationViewModel : BindableBase
 {
+    public int _claimedFunds = 0;
+    public int _actualFunds = 0;
+    public int _attractiveness = 10;
+    public int _fame = 0;
+    public int _suspicion = 5;
+
+    public int ClaimedFunds
+    {
+        get => _claimedFunds;
+        set => SetProperty(ref _claimedFunds, value);
+    }
+
+    public int ActualFunds
+    {
+        get => _actualFunds;
+        set => SetProperty(ref _actualFunds, value);
+    }
+
+    public int Attractiveness
+    {
+        get => _attractiveness;
+        set => SetProperty(ref _attractiveness, value);
+    }
+
+    public int Fame
+    {
+        get => _fame;
+        set => SetProperty(ref _fame, value);
+    }
+
+    public int Suspicion
+    {
+        get => _suspicion;
+        set => SetProperty(ref _suspicion, value);
+    }
 }
