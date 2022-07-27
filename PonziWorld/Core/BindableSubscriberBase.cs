536968189adb495bcbee96a54abbc1e5ef195261
Simon Palmer <sipalmer123@gmail.com>
2022-07-27 17:42:44 +0100

    Introduce BindableSubscribeBase to handle subscribing to processes

---
+++ PonziWorld/Core/BindableSubscriberBase.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+using System;
+using System.Threading.Tasks;
+
+namespace PonziWorld.Core;
+
+internal abstract class BindableSubscriberBase : BindableBase
+{
+    private readonly IEventAggregator eventAggregator;
+
+    protected BindableSubscriberBase(IEventAggregator eventAggregator) =>
+        this.eventAggregator = eventAggregator;
+
+    public void SubscribeToProcess<TEvent, TEventPayload, TCommand, TCommandPayload>(
+        Events.SagaProcess<TEvent, TEventPayload, TCommand, TCommandPayload> _,
+        Func<TCommandPayload, Task<TEventPayload>> function)
+        where TEvent : PubSubEvent<TEventPayload>, new()
+        where TCommand : PubSubEvent<TCommandPayload>, new() =>
+        eventAggregator.GetEvent<TCommand>()
+            .SubscribeAsync(commandPayload => GetProcessFunction<TEvent, TEventPayload, TCommandPayload>(function, commandPayload));
+
+    private async Task GetProcessFunction<TEvent, TEventPayload, TCommandPayload>(
+        Func<TCommandPayload, Task<TEventPayload>> function,
+        TCommandPayload payload)
+        where TEvent : PubSubEvent<TEventPayload>, new()
+    {
+        TEventPayload eventPayload = await function(payload);
+        eventAggregator.GetEvent<TEvent>().Publish(eventPayload);
+    }
+}
--- PonziWorld/DataRegion/InvestmentSummaryTabs/WithdrawersTab/WithdrawersTabViewModel.cs
+++ PonziWorld/DataRegion/InvestmentSummaryTabs/WithdrawersTab/WithdrawersTabViewModel.cs
@@ -1,10 +1,8 @@
-﻿using PonziWorld.Company;
-using PonziWorld.Core;
+﻿using PonziWorld.Core;
 using PonziWorld.Events;
 using PonziWorld.Investments;
 using PonziWorld.Investments.Investors;
 using Prism.Events;
-using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -13,10 +11,9 @@ using System.Threading.Tasks;
 
 namespace PonziWorld.DataRegion.InvestmentSummaryTabs.WithdrawersTab;
 
-internal class WithdrawersTabViewModel : BindableBase
+internal class WithdrawersTabViewModel : BindableSubscriberBase
 {
     private readonly IInvestorsRepository investorsRepository;
-    private readonly IEventAggregator eventAggregator;
     private ObservableCollection<DetailedInvestment> _withdrawals = new();
 
     public ObservableCollection<DetailedInvestment> Withdrawals
@@ -28,18 +25,17 @@ internal class WithdrawersTabViewModel : BindableBase
     public WithdrawersTabViewModel(
         IInvestorsRepository investorsRepository,
         IEventAggregator eventAggregator)
+        : base(eventAggregator)
     {
         this.investorsRepository = investorsRepository;
-        this.eventAggregator = eventAggregator;
 
-        eventAggregator.GetEvent<LoadWithdrawalsCommand>()
-            .SubscribeAsync(LoadWithdrawals);
+        SubscribeToProcess(LoadWithdrawals.Process, LoadLatestWithdrawals);
 
         eventAggregator.GetEvent<NewMonthInvestmentsGeneratedEvent>()
             .SubscribeAsync(CompileWithdrawalListAsync);
     }
 
-    private async Task LoadWithdrawals(LoadWithdrawalsCommandPayload payload)
+    private async Task<WithdrawalsLoadedEventPayload> LoadLatestWithdrawals(LoadWithdrawalsCommandPayload payload)
     {
         IEnumerable<Investment> lastMonthWithdrawals = payload.LastMonthInvestments
             .Where(investment => investment.Amount < 0);
@@ -47,7 +43,7 @@ internal class WithdrawersTabViewModel : BindableBase
         IEnumerable<DetailedInvestment> investments = await GetDetailedWithdrawalsAsync(lastMonthWithdrawals);
         SetWithdrawalsList(investments);
 
-        eventAggregator.GetEvent<WithdrawalsLoadedEvent>().Publish(new());
+        return new();
     }
 
     private async Task CompileWithdrawalListAsync(NewInvestmentsSummary investmentsSummary)
